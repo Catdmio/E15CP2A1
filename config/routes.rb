7d1e9009263974540f81a0cf98eeb73ee4331793
Catdmio <montecinos.catalina@gmail.com>
2018-04-23 00:08:16 -0300

    updating to repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :histories
-
-  root "histories#index"
+	root "histories#index"
+	devise_for :users, controllers: {registrations: 'users/registrations'}
+	resources :histories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
